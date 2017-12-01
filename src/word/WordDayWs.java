cc1239f8e643e21043aaf5f005fcea47c543b616
marquez2012 <jmmarques83@gmail.com>
2017-12-01 21:14:58 +0000

    Initial commit

---
+++ src/word/WordDayWs.java
@@ -0,0 +1,16 @@
+package word;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+ //http://www.superglobals.net/java-ee-soap-example/
+//"C:\Program Files\Java\jdk1.8.0_111\bin\wsgen.exe" -verbose -cp . word.WordDayWs -wsdl
+//wsgen -verbose -cp . word.WordDayWs -wsdl
+//http://localhost:8080/words-api-ws-0.0.1-SNAPSHOT/WordDayWs?wsdl
+@WebService
+public class WordDayWs {
+
+	@WebMethod
+    public String word() {
+        return "sadsa";
+    }
+}
